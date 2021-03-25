3ebb926c9f66a9d840def083e0c05e4d97277817
yujiekoh <kohyujie95@gmail.com>
2021-03-25 18:51:31 +0800

    create react app

---
+++ coolcrafts/src/App.js
@@ -0,0 +1,22 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  const api_key = `${process.env.REACT_APP_API_KEY}`;
+
+  console.log("api key");
+
+  const url = `https://openapi.etsy.com/v2/listings/967023146?api_key=${process.env.REACT_APP_API_KEY}`;
+
+  fetch(url)
+    .then(response => response.json())
+    .then(json => console.log(json));
+
+  return (
+    <div className="App">
+      <h1>CoolCrafts</h1>
+    </div>
+  );
+}
+
+export default App;
